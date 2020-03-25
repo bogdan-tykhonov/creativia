6d3abd225bc7b977a1922624cf2d128708935dd9
cryptoman423 <bogdan.v.t@ukr.net>
2020-03-25 21:05:58 +0200

    added  modal window, button-top, slider.

--- psd from internet/main.js
+++ psd from internet/main.js
@@ -1,8 +1,19 @@
+'use srict'
+//////////scrollTop/////////////////////
+const scrollBtn = document.querySelector('.arrow-top');
+scrollBtn.onclick = function(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+
 ///////dropdowntext///////////
-let text = Array.from(document.querySelectorAll('.textunderbtn'));
-let btnPlus = Array.from(document.querySelectorAll('.btn-plus'));
-let redText = Array.from(document.querySelectorAll('.btn-plus a'));
-let imgMinus = Array.from(document.querySelectorAll('.btn-plus img'));
+const text = Array.from(document.querySelectorAll('.textunderbtn'));
+const btnPlus = Array.from(document.querySelectorAll('.btn-plus'));
+const redText = Array.from(document.querySelectorAll('.btn-plus a'));
+const imgMinus = Array.from(document.querySelectorAll('.btn-plus img'));
 adding.onclick = function(event){
     let target = event.target;
     function Showtext(a){
@@ -27,4 +38,100 @@ adding.onclick = function(event){
         Showtext(2);
       }
 }
+///////////////slider/////////
+const images = document.querySelectorAll('.slider img');
+const imgArray = [];
+const textImg = document.querySelectorAll('.amr');
+const textArray = [];
+for(let i=0;i<images.length;i++){
+    imgArray[i]= images[i].src;
+    images[i].remove();
+    textArray[i] = textImg[i].textContent;
+    textImg[i].remove();
+}
 
+let step = 0;
+let offset = 0;
+function draw(){
+let div = document.createElement('div');
+div.className = 'slider-content';
+let img = document.createElement('img');
+img.src = imgArray[step];
+let text = document.createElement('a');
+text.className = 'amr';
+text.textContent = textArray[step];
+slider.appendChild(div);
+div.appendChild(img);
+div.appendChild(text);
+div.style.left= offset*150+'px';
+if(step+1 ==imgArray.length){
+    step = 0;
+}
+else{
+    step++;
+}
+offset=1;
+}
+draw();
+draw();
+ function ShowNext(){
+    let offset2 = 0;
+    draw();
+    let divArray = Array.from(document.querySelectorAll('.slider-content'));
+    for(let i=0;i<divArray.length;i++){
+divArray[i].style.left = offset2*150 - 150 +'px';
+offset2++;
+    }
+    setTimeout(function(){
+  divArray[0].remove();
+    },1000);
+    
+}
+setInterval(ShowNext, 3000);
+/////////////////SUbmit Form//////////////////////////
+function nameCheck(){
+    let name = document.querySelector('#name');
+    if(name.value.length<6){
+        nameRule.style.display = "block";
+        return false;
+    }
+    else{
+        nameRule.style.display = "";
+        return true;
+    }
+}
+function emailCheck(){
+    let email = document.querySelector('#email');
+    if(email.value.includes('@')){
+        emailRule.style.display = "";
+        return true;
+    }
+    else{
+        emailRule.style.display = "block";
+return false;
+    }
+}
+function subjectCheck(){
+    let subject = document.querySelector('#subject');
+    if(subject.value.length<6){
+        subjectRule.style.display = "block";
+        return false;
+    }
+    else{
+        subjectRule.style.display = "";
+        return true;
+    }
+}
+
+let modal = document.querySelector('.modal');
+submitBtn.onclick = function(){
+   if(nameCheck() == true && emailCheck()==true && subjectCheck()==true){
+      modal.style.display = "block";
+   }
+}
+document.onclick = function(event){
+    let target = event.target;
+    if(target.className =="close"||target.className =="close-btn"){
+        modal.style.display = "";
+    }
+}
